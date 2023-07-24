dc7c609a73604bdff754cb4dca75b1f9123b6327
RickDeb2004 <debanjanrick04@gmail.com>
2023-07-24 09:28:13 +0530

    issues resolved

--- bot/utility.go
+++ bot/utility.go
@@ -7,11 +7,13 @@ func DelMessages(session *discordgo.Session, message *discordgo.MessageCreate){
 	channelID:=message.ChannelID
 	limit:=100
 	 messageContent:=fmt.Sprintf("last messgaes %d can be purged", limit)
-	messages,err:=session.ChannelMessageSend(channelID,messageContent)
+	 _,err:=session.ChannelMessageSend(channelID,messageContent)
+	
 if err!=nil{
 	session.ChannelMessageSend(channelID,"Failed")
 	return
 }
+messages,err:=session.ChannelMessages(channelID,limit,"", "", "")
 var messageIDs []string
 for _,message:=range messages{
 	messageIDs=append(messageIDs,message.ID)
