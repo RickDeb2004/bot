a6a9ccb6f747d5d301e2333cb123e8367f3375fd
RickDeb2004 <debanjanrick04@gmail.com>
2023-08-06 11:20:05 +0530

    new feature added

---
+++ bot/webhook.go
@@ -0,0 +1,43 @@
+package bot
+
+import (
+	"encoding/json"
+	"fake-bot/config"
+	"fmt"
+	"io"
+	"net/http"
+	"github.com/bwmarrin/discordgo"
+)
+type GithubWebPayLoad struct{
+
+	Repository struct{
+		Name string `json:"name"`
+	}`json:"repository"`
+}
+func WebhookHandler(w http.ResponseWriter, r *http.Request){
+	body,err:=io.ReadAll(r.Body)
+	if err!=nil{
+
+		http.Error(w,"Failed to read",http.StatusInternalServerError)
+		return
+	}
+var payLoad GithubWebPayLoad
+	err=json.Unmarshal(body,&payLoad)
+	if err!=nil{
+		http.Error(w,"Failed to unmarshal",http.StatusInternalServerError)
+		return
+	}
+	session,err:=discordgo.New("Bot "+ config.Token)
+	if err!=nil{
+		fmt.Println(err.Error())
+		return
+	}
+	message:=fmt.Sprintf("New push to GitHub Repository :%s",payLoad.Repository.Name)
+	session.ChannelMessageSend(Discord_Channel_ID,message)
+	session.Close()
+	w.WriteHeader(http.StatusOK)
+
+
+
+
+}
